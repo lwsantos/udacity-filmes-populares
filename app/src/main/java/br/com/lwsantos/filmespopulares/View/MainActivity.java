980e1b5e9003a74da6d55c5669ba40be41b43a8c
Lincoln Watanabe <lwsantos@lwsantos.com.br>
2017-03-30 00:10:43 -0300

    - Troca do menu de configuracão por um spinner para realizar a ordem de classificacão do filme (por popularidade, por avaliacão, favoritos).

--- app/src/main/java/br/com/lwsantos/filmespopulares/View/MainActivity.java
+++ app/src/main/java/br/com/lwsantos/filmespopulares/View/MainActivity.java
@@ -1,9 +1,7 @@
 package br.com.lwsantos.filmespopulares.View;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
-import android.view.Menu;
 import android.view.MenuItem;
 
 import br.com.lwsantos.filmespopulares.R;
@@ -19,30 +17,5 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onResume() {
         super.onResume();
-        //carregarFilmes();
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        int id = item.getItemId();
-
-        switch (item.getItemId())
-        {
-            case R.id.men_settings:
-                Intent it = new Intent(this, SettingsActivity.class);
-                startActivity(it);
-                return true;
-            case R.id.men_atualizar:
-                //carregarFilmes();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
     }
 }
--- app/src/main/java/br/com/lwsantos/filmespopulares/View/MoviesFragment.java
+++ app/src/main/java/br/com/lwsantos/filmespopulares/View/MoviesFragment.java
@@ -2,18 +2,21 @@ package br.com.lwsantos.filmespopulares.View;
 
 import android.content.Context;
 import android.content.Intent;
-import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.os.Bundle;
-import android.preference.PreferenceManager;
 import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.GridView;
+import android.widget.Spinner;
 import android.widget.Toast;
 
 import java.util.ArrayList;
@@ -27,12 +30,15 @@ import br.com.lwsantos.filmespopulares.R;
 public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
 
     private ImageAdapter mAdapter;
-
     private GridView mGrdFilme;
+    private Spinner mSpnClassificacao;
+
     private static final String KEY_POSICAO_CLIQUE = "POSITION";
+    private static final String KEY_CLASSIFICACAO = "CLASSIFICACAO";
 
     // Declarar a variavel como static permite que o valor consiga ser recuperado após voltar da tela de Detalhes
     private static int mPosicaoClique = -1;
+    private static String mClassificacaoSaved = "";
 
     public MoviesFragment() {
         // Required empty public constructor
@@ -41,6 +47,8 @@ public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Sinaliza que o fragment possui um menu
+        setHasOptionsMenu(true);
     }
 
     @Override
@@ -64,6 +72,21 @@ public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
 
                 //Grava a posição do clique para ser utilizado quando retornar
                 mPosicaoClique = position;
+                mClassificacaoSaved = mSpnClassificacao.getSelectedItem().toString();
+            }
+        });
+
+        mSpnClassificacao = (Spinner) rootView.findViewById(R.id.spnClassificacao);
+        mSpnClassificacao.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                //Não é necessário pegar o item selecionado, pois ao carregar filmes ele captura essa informação
+                carregarFilmes();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
             }
         });
 
@@ -73,6 +96,20 @@ public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
             {
                 mPosicaoClique = savedInstanceState.getInt(KEY_POSICAO_CLIQUE);
             }
+
+            if(savedInstanceState.containsKey(KEY_CLASSIFICACAO))
+            {
+                mClassificacaoSaved = savedInstanceState.getString(KEY_CLASSIFICACAO);
+                // Define variavel que contém os array de valores do Spinner
+                TypedArray spinnerValues = getResources().obtainTypedArray(R.array.pref_classificacao_values);
+                for (int i=0; i < spinnerValues.length(); i++) {
+                    if(spinnerValues.getString(i).toString().equals(mClassificacaoSaved))
+                    {
+                        mSpnClassificacao.setSelection(1);
+                        break;
+                    }
+                }
+            }
         }
 
         return rootView;
@@ -81,7 +118,38 @@ public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
     @Override
     public void onResume() {
         super.onResume();
+
+        // Define variavel que contém os array de valores do Spinner
+        TypedArray spinnerEntries = getResources().obtainTypedArray(R.array.pref_classificacao_entries);
+        for (int i=0; i < spinnerEntries.length(); i++) {
+            if(spinnerEntries.getString(i).toString().equals(mClassificacaoSaved))
+            {
+                mSpnClassificacao.setSelection(i);
+                break;
+            }
+        }
+
         carregarFilmes();
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.menu_main, menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        switch (item.getItemId())
+        {
+            case R.id.men_atualizar:
+                carregarFilmes();
+                Toast.makeText(getContext(), getString(R.string.msg_atualizado), Toast.LENGTH_LONG).show();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 
     @Override
@@ -91,6 +159,8 @@ public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
         if(mPosicaoClique != mGrdFilme.INVALID_POSITION) {
             outState.putInt(KEY_POSICAO_CLIQUE, mPosicaoClique);
         }
+
+        outState.putString(KEY_CLASSIFICACAO, mSpnClassificacao.getSelectedItem().toString());
     }
 
     /**
@@ -130,11 +200,10 @@ public class MoviesFragment extends Fragment implements AsyncTaskDelegate {
     private void carregarFilmes(){
 
         if(verificarConexao()) {
-            //Captura as configuracao do aplicativo definido no SettingsActivity
-            SharedPreferences sharedPreferences = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            //Captura a ordem de classificacao configurada no aplicativo
-            String classificacao = sharedPreferences.getString(getString(R.string.pref_classificacao_key), getString(R.string.pref_classificacao_default));
-
+            // Define variavel que contém os array de valores do Spinner
+            TypedArray spinnerValues = getResources().obtainTypedArray(R.array.pref_classificacao_values);
+            // De acordo com o posição do item selecionado, caputara o valor do array.
+            String classificacao = spinnerValues.getString(mSpnClassificacao.getSelectedItemPosition());
             //Executa a thread em segundo plano para capturar a lista de filmes
             new TheMovieDBAsync(this, getContext()).execute(classificacao);
         }
